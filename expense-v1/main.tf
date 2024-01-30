b361fc47a5fd930486f61676a2c302e075752e3d
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-29 21:07:48 -0500

    tf code

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami                    = ami-0f3c7d07486cad139
+  ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-04e445f50c19e84f2"]
 
@@ -12,7 +12,7 @@ resource "aws_instance" "frontend" {
 
 
 resource "aws_instance" "backend" {
-  ami                    = ami-0f3c7d07486cad139
+  ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-04e445f50c19e84f2"]
 
@@ -24,7 +24,7 @@ resource "aws_instance" "backend" {
 
 
 resource "aws_instance" "mysql" {
-  ami                    = ami-0f3c7d07486cad139
+  ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-04e445f50c19e84f2"]
 
