292894518629fa1b2cf788b5da15824715459715
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-29 20:02:42 -0500

    tf code

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,3 +1,19 @@
 output "v1" {
   value = var.v1
 }
+
+output "first_value_in_list" {
+  value = var.v2[0]
+}
+
+output "second_value_in_list" {
+  value = var.v2[1]
+}
+
+output "third_value_in_list" {
+  value = var.v2[2]
+}
+
+output "value_in_map" {
+  value = var.v3[abc]
+}
--- 02-vars/variables.tf
+++ 02-vars/variables.tf
@@ -1,3 +1,14 @@
 variable "v1" {
   default = "hello world"
 }
+
+variable "v2" {
+  default = ["hello world", 100, true]
+}
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "two"
+  }
+}
