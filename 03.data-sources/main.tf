03fe13e7d6753ce399dab6ef3dcb3ede6097bb87
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-31 14:56:47 -0500

    tf code

---
+++ 03.data-sources/main.tf
@@ -0,0 +1,11 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+
+
+output "aws-ami" {
+  value = data.aws_ami.example.image_id
+}
