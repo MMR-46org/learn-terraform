a5314379d58efe230088d8b77c549a1585b60f38
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-31 15:05:19 -0500

    tf code

---
+++ expense-v2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_id" {
+  default = ["sg-04e445f50c19e84f2"]
+}
+
+variable "zone-id" {
+  default = "Z0189341LG4L24HIU4QF"
+}
