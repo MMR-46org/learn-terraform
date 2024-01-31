ea09cb7213d5d5c79f50eced0c39a6fc5a02d874
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-31 18:48:10 -0500

    tf code

--- 05-loops-2/main.tf
+++ 05-loops-2/main.tf
@@ -1,39 +1,39 @@
-variable  "fruits1" {
-  default = {
-    apple    = 10
-    banana   = 20
-  }
-}
-
-
-
-#variable  "fruits2" {
-#  default    =   {
-#    apple = {
-#      name   = "apple"
-#      quantity = 10
-#    }
-#  banana =  {
-#    name   = "banana"
-#    quantity  = 20
+#variable  "fruits1" {
+#  default = {
+#    apple    = 10
+#    banana   = 20
 #  }
-#
-#  }
-#
 #}
 
 
-resource "null_resource" "test" {
-  for_each = var.fruits1
-  provisioner "local-exec" {
-    command = "echo ${each.key} = ${each.value}"
+
+variable  "fruits2" {
+  default    =   {
+    apple = {
+      name   = "apple"
+      quantity = 10
+    }
+  banana =  {
+    name   = "banana"
+    quantity  = 20
   }
+
+  }
+
 }
 
 
-#resource "null_resource" "test1" {
-#  for_each = var.fruits2
+#resource "null_resource" "test" {
+#  for_each = var.fruits1
 #  provisioner "local-exec" {
-#    command = "echo ${lookup(each.value, "name" , "null")} = ${lookup(each.value, "name" , "null")}"
+#    command = "echo ${each.key} = ${each.value}"
 #  }
 #}
+
+
+resource "null_resource" "test1" {
+  for_each = var.fruits2
+  provisioner "local-exec" {
+    command = "echo ${lookup(each.value, "name" , "null")} = ${lookup(each.value, "name" , "null")}"
+  }
+}
