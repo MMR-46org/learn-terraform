23a29689fc78842e9df8ae989bb340682e70f741
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-31 16:55:57 -0500

    tf code

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -1,8 +1,24 @@
-resource "null_resource" "test" {
-  count  = 10
+#resource "null_resource" "test" {
+#  count  = 10
+#}
+#
+#
+#output "test" {
+#  value = null_resource.test.*.id
+#}
+
+
+variable  "components" {
+  default = ["frontend", "backend", "mysql"]
 }
 
+resource "aws_instance" "test" {
+  count                  = length(var.components)
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-04e445f50c19e84f2"]
 
-output "test" {
-  value = null_resource.test.*.id
+  tags = {
+    Name = element(var.components , count.index)
+  }
 }
